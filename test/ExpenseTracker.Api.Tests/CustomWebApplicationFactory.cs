ab693ed664f868d574cef149bf50499dde7e73d7
Alexandra Mourato <X220012@bcpcorp.net>
2026-05-25 17:39:57 +0100

    Refactor: use IExpenseTrackerDbContext instead of ExpenseTrackerDbContext

--- test/ExpenseTracker.Api.Tests/CustomWebApplicationFactory.cs
+++ test/ExpenseTracker.Api.Tests/CustomWebApplicationFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace ExpenseTracker.Api.Tests;
 
